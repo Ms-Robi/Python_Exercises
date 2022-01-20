13938c541396b749dedb564b8c3e867096d5a907
Ms-Robi <36694735+Ms-Robi@users.noreply.github.com>
2022-01-20 14:16:42 +0300

    Update Characterr_Picture_Grid.py

--- Characterr_Picture_Grid.py
+++ Characterr_Picture_Grid.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
-# coding: utf-8
+Result:
+         
+. . O O . O O . . 
+. O O O O O O O . 
+. O O O O O O O . 
+. . O O O O O . . 
+. . . O O O . . . 
+. . . . O . . . . 
 
-# In[10]:
-
-
+# Method 1: Using Zip
 grid = [['.', '.', '.', '.', '.', '.'],
          ['.', 'O', 'O', '.', '.', '.'],
          ['O', 'O', 'O', 'O', '.', '.'],
@@ -16,6 +21,11 @@ grid = [['.', '.', '.', '.', '.', '.'],
     
 for i in zip(*grid):
  print(*i)
-    
-    
+  
+# Method 2: Using Loops
+
+for i in range(6):
+    for j in range(9):
+        print(grid[j][i], end = " ")
+    print()
 
